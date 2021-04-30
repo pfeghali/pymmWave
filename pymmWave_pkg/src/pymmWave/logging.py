5f5b8f74f7504c24269c604da417d64464beb128
Peter Feghali <peterfeghali99@gmail.com>
2021-04-30 13:40:04 -0700

    Added native logging support

--- pymmWave_pkg/src/pymmWave/logging.py
+++ pymmWave_pkg/src/pymmWave/logging.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Any
+import logging as log
 
 class Logger(ABC):
     """Base abstract class for all algorithms.
@@ -20,8 +21,19 @@ class Logger(ABC):
         pass
 
 class StdOutLogger(Logger):
+    """Very simple print based logger.
+    """
     def log(self, *args: Any, **kwargs: Any) -> None:
         print(*args, **kwargs)
 
     def error(self, *args: Any, **kwargs: Any) -> None:
         print('\033[91m', *args, '\033[0m', **kwargs)
+
+class NativeLogger(Logger):
+    """Logger using python's logging library. User can set settings out of scope which will persist.
+    """
+    def log(self, *args: Any, **kwargs: Any) -> None:
+        log.info(*args, **kwargs)
+
+    def error(self, *args: Any, **kwargs: Any) -> None:
+        log.error(*args, **kwargs)
